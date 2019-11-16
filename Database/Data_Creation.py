0487f2d1676891bf969e1a327d5d24bd31a6664c
AlbertoPascal <beto_pascal@hotmail.com>
2019-11-16 17:34:13 -0600

    Updated script to generate sutdents and teachers seperately. Included insert scripts for both

--- Database/Data_Creation.py
+++ Database/Data_Creation.py
@@ -11,12 +11,21 @@ from faker import Faker
 
 faker = Faker()
 stdnt_str = 'INSERT INTO STUDENTS (STUDENT_ID, NAMES, LASTNAMES, MAIL, PASS) VALUES ('
-f = open('Bulk_inserts.txt', 'w')
-for i in range(0,50):
+teacher_str = 'INSERT INTO TEACHERS (TEACHER_ID, NAMES, LASTNAMES, MAIL, PASS) VALUES ('
+f = open('Student_Inserts.txt', 'w')
+for i in range(0,150):
     r = randint(111111,999999)
     ID = "A01" + str(r)
     insert_str = stdnt_str + "'" + str(ID) + "','" + faker.first_name() +  "','" + faker.last_name() + " " + faker.last_name() + "','" + str(ID) + "@tec.mx','" + faker.word() + "');\n"
     f.write(insert_str)
+    
+f.close()
 
+f = open('Teacher_Inserts.txt', 'w')
+for i in range(0,20):
+    r = randint(111111,999999)
+    ID = "L01" + str(r)
+    insert_str = teacher_str + "'" + str(ID) + "','" + faker.first_name() +  "','" + faker.last_name() + " " + faker.last_name() + "','" + str(ID) + "@tec.mx','" + faker.word() + "');\n"
+    f.write(insert_str) 
     
 f.close()
