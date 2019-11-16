41e446f1e9b0403eb9f3011bad1a6b9c8f08711b
ManuGuV <manologuadarramav@hotmail.com>
2019-11-16 17:38:31 -0600

    Changes to table

--- proyecto-calidad/src/Home-page/Alumnos.js
+++ proyecto-calidad/src/Home-page/Alumnos.js
@@ -50,42 +50,43 @@ class tablaAlumnos extends React.Component {
     
 
     return (
-      <Paper >
-        <Table id="students">
-          <TableHead>
-            <TableRow>
-              <TableCell>ID</TableCell>
-              <TableCell >Name</TableCell>
-              <TableCell >Surname</TableCell>
-              <TableCell >Class</TableCell>
-              <TableCell >Academic</TableCell>
-              <TableCell >Team Work</TableCell>
-              <TableCell >Communication Skills</TableCell>
-              <TableCell > </TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {this.alumnos.map(alumnos => (
-              <TableRow key={alumnos.id}>
-                <TableCell component="th" scope="row">{alumnos.id}</TableCell>
-                <TableCell >{alumnos.name}</TableCell>
-                <TableCell >{alumnos.surname}</TableCell>
-                <TableCell >{alumnos.enrolledClasses[0].class}</TableCell>
-                <TableCell >
-                  <input type="number" id={"grade1" + alumnos.id} min="0" max="100"/>
-                </TableCell>
-                <TableCell >
-                  <input type="number" id={"grade2" + alumnos.id} min="0" max="100"/>
-                </TableCell>
-                <TableCell >
-                  <input type="number" id={"grade3" + alumnos.id} min="0" max="100"/>
-                </TableCell>
+      <div>
+        <Paper >
+          <Table id="students">
+            <TableHead>
+              <TableRow>
+                <TableCell>ID</TableCell>
+                <TableCell >Name</TableCell>
+                <TableCell >Surname</TableCell>
+                <TableCell >Class</TableCell>
+                <TableCell >Academic</TableCell>
+                <TableCell >Team Work</TableCell>
+                <TableCell >Communication Skills</TableCell>
               </TableRow>
-            ))}
-          </TableBody>
-        </Table>
+            </TableHead>
+            <TableBody>
+              {this.alumnos.map(alumnos => (
+                <TableRow key={alumnos.id}>
+                  <TableCell component="th" scope="row">{alumnos.id}</TableCell>
+                  <TableCell >{alumnos.name}</TableCell>
+                  <TableCell >{alumnos.surname}</TableCell>
+                  <TableCell >{alumnos.enrolledClasses[0].class}</TableCell>
+                  <TableCell >
+                    <input type="number" id={"grade1" + alumnos.id} min="0" max="100"/>
+                  </TableCell>
+                  <TableCell >
+                    <input type="number" id={"grade2" + alumnos.id} min="0" max="100"/>
+                  </TableCell>
+                  <TableCell >
+                    <input type="number" id={"grade3" + alumnos.id} min="0" max="100"/>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </Paper>
         <Button className="btn btn-primary" variant="contained" onClick={() => this.updateGrade()}>Submit</Button>
-      </Paper>
+      </div>
     );
   }
 }
