507a471fd4995ad145268bb444907c9681379f4e
AlbertoPascal <beto_pascal@hotmail.com>
2019-11-16 18:12:51 -0600

    Updated Data creation script to now include classes. Also added the Class_insert script

--- Database/Data_Creation.py
+++ Database/Data_Creation.py
@@ -12,6 +12,11 @@ from faker import Faker
 faker = Faker()
 stdnt_str = 'INSERT INTO STUDENTS (STUDENT_ID, NAMES, LASTNAMES, MAIL, PASS) VALUES ('
 teacher_str = 'INSERT INTO TEACHERS (TEACHER_ID, NAMES, LASTNAMES, MAIL, PASS) VALUES ('
+class_str = 'INSERT INTO CLASS(CLASS_ID, CLASS_NAME) VALUES ('
+
+Letters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+Topics = ['Math', 'Biology', 'Ethics', 'Computing', 'Entrepreneurship', 'Leadership']
+Topics2 = ['Behaviour', 'in a Business Environment', 'Introduction to', 'I', 'II', 'III', 'IV']
 f = open('Student_Inserts.txt', 'w')
 for i in range(0,150):
     r = randint(111111,999999)
@@ -31,3 +36,24 @@ for i in range(0,20):
     f.write(insert_str) 
     
 f.close()
+
+
+f = open('Class_Inserts.txt', 'w')
+for i in range(0,30):
+    r = randint(1000,9999)
+    rand_letter = randint(0,25)
+    rand_letter2 = randint(0,25)
+    rand_topic = randint(0,5)
+    rand_topic2 = randint(0,6)
+    rand_str = ""
+    if rand_topic2 == 2:
+        rand_str = str(Topics2[rand_topic2]) + ' ' + str(Topics[rand_topic])
+    else:
+        rand_str =str(Topics[rand_topic]) + ' ' + str(Topics2[rand_topic2]) 
+        
+    ID =str(Letters[rand_letter]) + str(Letters[rand_letter2]) + str(r)
+    insert_str = class_str + "'" + ID + "','" + rand_str +  "');\n"
+    f.write(insert_str) 
+    
+f.close()
+
