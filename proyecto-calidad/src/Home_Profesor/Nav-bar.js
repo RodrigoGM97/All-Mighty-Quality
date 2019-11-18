20843e314e7d218cf566d7c32afe5db857ed0b16
RodrigoGM97 <rodrigogm971@gmail.com>
2019-11-17 21:03:39 -0600

    asdf

--- proyecto-calidad/src/Home_Profesor/Nav-bar.js
+++ proyecto-calidad/src/Home_Profesor/Nav-bar.js
@@ -20,28 +20,41 @@ import axios from 'axios';
     getData(teacher_id) {
         axios.get("http://localhost:5000/getClassesofTeacher?teacher-id="+teacher_id).then(response => {
           this.props.setClasses(response);
-        })
-      }
+          
+        });
+    }
+    sleep = (milliseconds) => {
+        return new Promise(resolve => setTimeout(resolve, milliseconds))
+    }
 
     render() {
         this.getData(this.props.state.currentUser);
         this.clases = this.props.state.classesArr;
-        console.log(this.props.state);
         
+        this.sleep(500).then(() => {
+            //do stuff
+          
+        console.log("hola: %j",this.props.state.classesArr);
+        var arr = this.props.state.classesArr;
+        console.log("Arr: %j",arr[0]);
+        console.log("Length: %j",arr);
+        
+        })
         return (
             <div>
                 <Navbar bg="light" expand="lg">
                     <img src={Logo} width="50" height="50" />
                     <Navbar.Brand href="#home" style = {{marginLeft: '15px'}}>International Exchange Portal</Navbar.Brand>
                     <DropdownButton  as={ButtonGroup} title="Groups" id="bg-vertical-dropdown-1" style = {{marginLeft: '15px'}} >
-                    {this.clases.map(clases => (
-                        <Dropdown.Item key={clases.id} id = {clases.id} onClick={() => this.updateGrade(clases.id)} >{clases.name}</Dropdown.Item>
+                    {this.clases.map(clasesArr => (
+                        <Dropdown.Item key={clasesArr.id} id = {clasesArr.id} onClick={() => this.updateGrade(clasesArr.id)} >{clasesArr.name}</Dropdown.Item>
                     ))}
                     </DropdownButton>
                     <Link className="btn btn-outline-success" variant="outline-success" style = {{marginLeft: 'auto'}} to="/">Sign out</Link>
                 </Navbar>
              </div>
         )
+        
     }
 
  }
@@ -53,7 +66,7 @@ import axios from 'axios';
   }
   
   const mapDispatchToProps = {
-    setClasses
+    setClasses,
   }
   
   export default connect(mapStateToProps, mapDispatchToProps)(NavBar);
--- proyecto-calidad/src/classes/Data.js
+++ proyecto-calidad/src/classes/Data.js
@@ -12,14 +12,13 @@ class Data {
     }
 
     setClasses(json_response) {
-    
+        
         for(var i=0;i<json_response.data.length;i++)
         {
             var classes = new Clase(json_response.data[i].className, json_response.data[i].classID);
             this.classesArr.push(classes);
         }
-        console.log("Arr: %j",this.classesArr);
-        
+        console.log("Data: %j", this.classesArr);
     }
 }
 
