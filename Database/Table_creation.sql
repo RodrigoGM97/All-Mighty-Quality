e928951c75a88d802cc70e1f30c4692750a53256
AlbertoPascal <beto_pascal@hotmail.com>
2019-11-28 20:41:51 -0600

    updated sql table creation scripts for error prevention and massive student update

--- Database/Table_creation.sql
+++ Database/Table_creation.sql
@@ -1,3 +1,5 @@
+
+
 BEGIN
 CREATE TABLE STUDENTS
 (
@@ -45,3 +47,109 @@ CREATE TABLE STUDENT_HAS_CLASS
     constraint ClASS_ID_TEACHER FOREIGN KEY (Class_id) REFERENCES CLASS(class_id)
 );
 END;
+
+
+CREATE TRIGGER GradeUPDATE ON [dbo].[STUDENT_HAS_CLASS]
+AFTER update
+AS
+BEGIN
+	UPDATE [dbo].[STUDENT_HAS_CLASS]
+	SET Academic_grade = 100
+	WHERE Academic_grade > 100;
+
+    UPDATE [dbo].[STUDENT_HAS_CLASS]
+	SET TeamWork_grade = 100
+	WHERE TeamWork_grade > 100;
+
+    UPDATE [dbo].[STUDENT_HAS_CLASS]
+	SET Communication_grade = 100
+	WHERE Communication_grade > 100;
+
+    UPDATE [dbo].[STUDENT_HAS_CLASS]
+	SET Final_grade = 100
+	WHERE Final_grade > 100;
+
+    UPDATE [dbo].[STUDENT_HAS_CLASS]
+	SET Academic_grade = 0
+	WHERE Academic_grade <0;
+
+    UPDATE [dbo].[STUDENT_HAS_CLASS]
+	SET TeamWork_grade = 0
+	WHERE TeamWork_grade <0
+
+    UPDATE [dbo].[STUDENT_HAS_CLASS]
+	SET Communication_grade = 0
+	WHERE Communication_grade < 0;
+
+    UPDATE [dbo].[STUDENT_HAS_CLASS]
+	SET Final_grade = (Academic_grade + Teamwork_grade + Communication_grade)/3
+	WHERE Final_grade < 0;
+END
+
+CREATE TRIGGER GradeINSERT ON [dbo].[STUDENT_HAS_CLASS]
+AFTER INSERT
+AS
+BEGIN
+	UPDATE [dbo].[STUDENT_HAS_CLASS]
+	SET Academic_grade = 100
+	WHERE Academic_grade > 100;
+
+    UPDATE [dbo].[STUDENT_HAS_CLASS]
+	SET TeamWork_grade = 100
+	WHERE TeamWork_grade > 100;
+
+    UPDATE [dbo].[STUDENT_HAS_CLASS]
+	SET Communication_grade = 100
+	WHERE Communication_grade > 100;
+
+    UPDATE [dbo].[STUDENT_HAS_CLASS]
+	SET Final_grade = 100
+	WHERE Final_grade > 100;
+
+    UPDATE [dbo].[STUDENT_HAS_CLASS]
+	SET Academic_grade = 0
+	WHERE Academic_grade <0;
+
+    UPDATE [dbo].[STUDENT_HAS_CLASS]
+	SET TeamWork_grade = 0
+	WHERE TeamWork_grade <0
+
+    UPDATE [dbo].[STUDENT_HAS_CLASS]
+	SET Communication_grade = 0
+	WHERE Communication_grade < 0;
+
+    UPDATE [dbo].[STUDENT_HAS_CLASS]
+	SET Final_grade = (Academic_grade + Teamwork_grade + Communication_grade)/3
+	WHERE Final_grade < 0;
+END
+
+-- massive student grade update:
+
+DECLARE @gradeA float
+DECLARE @gradeB float
+DECLARE @gradeC float
+DECLARE @class varchar(100)
+DECLARE @student varchar(100)
+
+DECLARE @newgradeA float
+DECLARE @newgradeB float
+DECLARE @newgradeC float
+DECLARE db_cursor CURSOR FOR 
+SELECT CLASS_ID, STUDENT_ID, Academic_grade, TeamWork_grade, Communication_grade
+from [dbo].[STUDENT_HAS_CLASS]
+
+OPEN db_cursor  
+FETCH NEXT FROM db_cursor INTO @class, @student, @gradeA, @gradeB, @gradeC  
+
+WHILE @@FETCH_STATUS = 0  
+BEGIN  
+      SET @newgradeA = FLOOR(RAND()*(121-34+1))+34
+      SET @newgradeB = FLOOR(RAND()*(121-34+1))+34
+      SET @newgradeC = FLOOR(RAND()*(121-34+1))+34
+      UPDATE [dbo].[STUDENT_HAS_CLASS] SET ACADEMIC_GRADE = @newgradeA, teamwork_grade = @newgradeB, communication_grade = @newgradeC, Final_grade=-1 where class_id = @class and student_id = @student
+
+      FETCH NEXT FROM db_cursor INTO @class, @student, @gradeA, @gradeB, @gradeC 
+END 
+
+CLOSE db_cursor  
+DEALLOCATE db_cursor 
